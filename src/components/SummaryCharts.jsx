398ddad613640376458fa94524c05f62c82a48ef
hoidal <sjhoidal@gmail.com>
2020-06-17 22:25:41 -0600

    added additional chart data and put all within tabbed contianer

--- src/components/SummaryCharts.jsx
+++ src/components/SummaryCharts.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import '../App.css';
 
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 import { CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Bar } from 'recharts';
 import { formatNum, formatDate } from '../helper-functions/formatters';
 
 function SummaryCharts({ data }) {
 
+    console.log(data)
     const formattedGraphData = data.reverse().map(dailyData => {
         const formattedData = {
             date: formatDate(dailyData.date),
             newCases: dailyData.positiveIncrease,
             totalCases: dailyData.positive,
             newDeaths: dailyData.deathIncrease,
-            totalDeaths: dailyData.death
-            
+            totalDeaths: dailyData.death,
+            recovered: dailyData.recovered,
+            totalTestResults: dailyData.totalTestResults
         }
         return formattedData;
     });
@@ -25,44 +30,62 @@ function SummaryCharts({ data }) {
     return (
         <div id="chart-container">
             <div>
-                <h5>Total Cases</h5>
-                <BarChart width={500} height={250} data={formattedGraphData}>
-                    <CartesianGrid strokeDasharray="3" vertical={false}/>
-                    <XAxis dataKey="date" />
-                    <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
-                    <Tooltip formatter={(data, name) => [formatNum(data), "Total Cases"]}/>
-                    <Bar dataKey="totalCases" fill="#8884d8" />
-                </BarChart>
-            </div>
-            <div>
-                <h5>Total Deaths</h5>
-                <BarChart width={500} height={250} data={filteredDeathData}>
-                    <CartesianGrid strokeDasharray="3" vertical={false}/>
-                    <XAxis dataKey="date" />
-                    <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
-                    <Tooltip formatter={(data, name) => [formatNum(data), "Total Deaths"]} />
-                    <Bar dataKey="totalDeaths" fill="red" />
-                </BarChart>
-            </div>
-            <div>
-                <h5>Daily Cases</h5>
-                <BarChart width={500} height={250} data={filteredDeathData}>
-                    <CartesianGrid strokeDasharray="3" vertical={false}/>
-                    <XAxis dataKey="date" />
-                    <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
-                    <Tooltip formatter={(data, name) => [formatNum(data), "Daily Cases"]} />
-                    <Bar dataKey="newCases" fill="#8884d8" />
-                </BarChart>
-            </div>
-            <div>
-                <h5>Daily Deaths</h5>
-                <BarChart width={500} height={250} data={filteredDeathData}>
-                    <CartesianGrid strokeDasharray="3" vertical={false}/>
-                    <XAxis dataKey="date" />
-                    <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
-                    <Tooltip formatter={(data, name) => [formatNum(data), "New Deaths"]} />
-                    <Bar dataKey="newDeaths" fill="red" />
-                </BarChart>
+                <Tabs defaultActiveKey="total-cases" id="chart-nav-bar">
+                    <Tab eventKey="total-cases" title="Total Cases">
+                        <BarChart width={1000} height={250} data={formattedGraphData}>
+                            <CartesianGrid strokeDasharray="3" vertical={false}/>
+                            <XAxis dataKey="date" />
+                            <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
+                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Cases"]}/>
+                            <Bar dataKey="totalCases" fill="#8884d8" />
+                        </BarChart>
+                    </Tab>
+                    <Tab eventKey="daily-cases" title="Daily Cases">
+                        <BarChart width={1000} height={250} data={filteredDeathData}>
+                            <CartesianGrid strokeDasharray="3" vertical={false}/>
+                            <XAxis dataKey="date" />
+                            <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
+                            <Tooltip formatter={(data, name) => [formatNum(data), "Daily Cases"]} />
+                            <Bar dataKey="newCases" fill="#8884d8" />
+                        </BarChart>
+                    </Tab>
+                    <Tab eventKey="total-deaths" title="Total Deaths">
+                        <BarChart width={1000} height={250} data={filteredDeathData}>
+                            <CartesianGrid strokeDasharray="3" vertical={false}/>
+                            <XAxis dataKey="date" />
+                            <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
+                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Deaths"]} />
+                            <Bar dataKey="totalDeaths" fill="red" />
+                        </BarChart>
+                    </Tab>
+                    <Tab eventKey="daily-deaths" title="Daily Deaths">
+                        <BarChart width={1000} height={250} data={filteredDeathData}>
+                            <CartesianGrid strokeDasharray="3" vertical={false}/>
+                            <XAxis dataKey="date" />
+                            <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
+                            <Tooltip formatter={(data, name) => [formatNum(data), "Daily Deaths"]} />
+                            <Bar dataKey="newDeaths" fill="red" />
+                        </BarChart>
+                    </Tab>
+                    <Tab eventKey="total-recoveries" title="Total Recovered">
+                        <BarChart width={1000} height={250} data={filteredDeathData}>
+                            <CartesianGrid strokeDasharray="3" vertical={false}/>
+                            <XAxis dataKey="date" />
+                            <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
+                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Recovered"]} />
+                            <Bar dataKey="recovered" fill="red" />
+                        </BarChart>
+                    </Tab>
+                    <Tab eventKey="total-test-results" title="Total Tests">
+                        <BarChart width={1000} height={250} data={filteredDeathData}>
+                            <CartesianGrid strokeDasharray="3" vertical={false}/>
+                            <XAxis dataKey="date" />
+                            <YAxis width={80} tickFormatter={tick => tick.toLocaleString()}/>
+                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Test Results"]} />
+                            <Bar dataKey="totalTestResults" fill="red" />
+                        </BarChart>
+                    </Tab>
+                </Tabs>
             </div>
         </div>
     )

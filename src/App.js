e0345df4d265f7d6d396c8c186c3d23ab953dec2
hoidal <sjhoidal@gmail.com>
2020-06-19 11:02:07 -0600

    added nav bar but need to format

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 import Container from 'react-bootstrap/Container';
+import MainNavBar from './components/MainNavBar';
 import USHomePage from './containers/USHomePage';
 import StateHomePage from './containers/StateHomePage';
 
 function App() {
   return (
-    <Container className="App">
-      <Switch>
-        <Route exact path='/' component={USHomePage} />
-        <Route exact path='/state/:stateName' component={StateHomePage} />
-      </Switch>
-    </Container>
+    <Fragment>
+      <MainNavBar />
+      <Container className="App">
+        <Switch>
+          <Route exact path='/' component={USHomePage} />
+          <Route exact path='/state/:stateName' component={StateHomePage} />
+        </Switch>
+      </Container>
+    </Fragment>
   );
 }
 

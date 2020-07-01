0173a67e0533b0ee45f0e1cdcc2e6005aec8bbd7
hoidal <sjhoidal@gmail.com>
2020-07-01 16:14:27 -0600

    added responsive container for data charts. need to add for news, table, and potentially header

--- src/components/DataCharts.jsx
+++ src/components/DataCharts.jsx
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
-import { CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Bar } from 'recharts';
+import { ResponsiveContainer, CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Bar } from 'recharts';
 import { formatNum, formatDate } from '../helper-functions/formatters';
 
 function DataCharts({ data }) {
@@ -28,58 +28,70 @@ function DataCharts({ data }) {
             <Card.Body>
                 <Tabs defaultActiveKey="daily-cases" className={styles.chartNavTabs}>
                     <Tab eventKey="daily-cases" title="Daily Cases">
-                        <BarChart width={1000} height={400} margin={{top: 50, right: 0, bottom: 10, left: 10}} data={formattedGraphData}>
-                            <CartesianGrid strokeDasharray="3" vertical={false}/>
-                            <XAxis dataKey="date" />
-                            <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
-                            <Tooltip formatter={(data, name) => [formatNum(data), "Daily Cases"]} />
-                            <Bar dataKey="newCases" fill="#8884d8" />
-                        </BarChart>
+                        <ResponsiveContainer width='100%' height={400}>
+                            <BarChart margin={{top: 50, right: 50, bottom: 10, left: 10}} data={formattedGraphData}>
+                                <CartesianGrid strokeDasharray="3" vertical={false}/>
+                                <XAxis dataKey="date" />
+                                <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
+                                <Tooltip formatter={(data, name) => [formatNum(data), "Daily Cases"]} />
+                                <Bar dataKey="newCases" fill="#8884d8" />
+                            </BarChart>
+                        </ResponsiveContainer>
                     </Tab>
                     <Tab eventKey="total-cases" title="Total Cases">
-                        <BarChart width={1000} height={400} margin={{top: 50, right: 0, bottom: 10, left: 10}} data={formattedGraphData}>
-                            <CartesianGrid strokeDasharray="3" vertical={false}/>
-                            <XAxis dataKey="date" />
-                            <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
-                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Cases"]}/>
-                            <Bar dataKey="totalCases" fill="#8884d8" />
-                        </BarChart>
+                        <ResponsiveContainer width='100%' height={400}>
+                            <BarChart margin={{top: 50, right: 50, bottom: 10, left: 10}} data={formattedGraphData}>
+                                <CartesianGrid strokeDasharray="3" vertical={false}/>
+                                <XAxis dataKey="date" />
+                                <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
+                                <Tooltip formatter={(data, name) => [formatNum(data), "Total Cases"]}/>
+                                <Bar dataKey="totalCases" fill="#8884d8" />
+                            </BarChart>
+                        </ResponsiveContainer>
                     </Tab>
                     <Tab eventKey="daily-deaths" title="Daily Deaths">
-                        <BarChart width={1000} height={400} margin={{top: 50, right: 0, bottom: 10, left: 10}} data={formattedGraphData}>
-                            <CartesianGrid strokeDasharray="3" vertical={false}/>
-                            <XAxis dataKey="date" />
-                            <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
-                            <Tooltip formatter={(data, name) => [formatNum(data), "Daily Deaths"]} />
-                            <Bar dataKey="newDeaths" fill="#f93e3e" />
-                        </BarChart>
+                        <ResponsiveContainer width='100%' height={400}>
+                            <BarChart margin={{top: 50, right: 50, bottom: 10, left: 10}} data={formattedGraphData}>
+                                <CartesianGrid strokeDasharray="3" vertical={false}/>
+                                <XAxis dataKey="date" />
+                                <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
+                                <Tooltip formatter={(data, name) => [formatNum(data), "Daily Deaths"]} />
+                                <Bar dataKey="newDeaths" fill="#f93e3e" />
+                            </BarChart>
+                        </ResponsiveContainer>
                     </Tab>
                     <Tab eventKey="total-deaths" title="Total Deaths">
-                        <BarChart width={1000} height={400} margin={{top: 50, right: 0, bottom: 10, left: 10}} data={formattedGraphData}>
-                            <CartesianGrid strokeDasharray="3" vertical={false}/>
-                            <XAxis dataKey="date" />
-                            <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
-                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Deaths"]} />
-                            <Bar dataKey="totalDeaths" fill="#f93e3e" />
-                        </BarChart>
+                        <ResponsiveContainer width='100%' height={400}>
+                            <BarChart margin={{top: 50, right: 50, bottom: 10, left: 10}} data={formattedGraphData}>
+                                <CartesianGrid strokeDasharray="3" vertical={false}/>
+                                <XAxis dataKey="date" />
+                                <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
+                                <Tooltip formatter={(data, name) => [formatNum(data), "Total Deaths"]} />
+                                <Bar dataKey="totalDeaths" fill="#f93e3e" />
+                            </BarChart>
+                        </ResponsiveContainer>
                     </Tab>
                     <Tab eventKey="total-recoveries" title="Total Recovered">
-                        <BarChart width={1000} height={400} margin={{top: 50, right: 0, bottom: 10, left: 10}} data={formattedGraphData}>
-                            <CartesianGrid strokeDasharray="3" vertical={false}/>
-                            <XAxis dataKey="date" />
-                            <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
-                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Recovered"]} />
-                            <Bar dataKey="recovered" fill="#82ca9d" />
-                        </BarChart>
+                        <ResponsiveContainer width='100%' height={400}>
+                            <BarChart margin={{top: 50, right: 50, bottom: 10, left: 10}} data={formattedGraphData}>
+                                <CartesianGrid strokeDasharray="3" vertical={false}/>
+                                <XAxis dataKey="date" />
+                                <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
+                                <Tooltip formatter={(data, name) => [formatNum(data), "Total Recovered"]} />
+                                <Bar dataKey="recovered" fill="#82ca9d" />
+                            </BarChart>
+                        </ResponsiveContainer>
                     </Tab>
                     <Tab eventKey="total-test-results" title="Total Tested">
-                        <BarChart width={1000} height={400} margin={{top: 50, right: 0, bottom: 10, left: 10}} data={formattedGraphData}>
-                            <CartesianGrid strokeDasharray="3" vertical={false}/>
-                            <XAxis dataKey="date" />
-                            <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
-                            <Tooltip formatter={(data, name) => [formatNum(data), "Total Test Results"]} />
-                            <Bar dataKey="totalTestResults" fill="#82ca9d" />
-                        </BarChart>
+                        <ResponsiveContainer width='100%' height={400}>
+                            <BarChart margin={{top: 50, right: 50, bottom: 10, left: 10}} data={formattedGraphData}>
+                                <CartesianGrid strokeDasharray="3" vertical={false}/>
+                                <XAxis dataKey="date" />
+                                <YAxis width={100} tickFormatter={tick => tick.toLocaleString()}/>
+                                <Tooltip formatter={(data, name) => [formatNum(data), "Total Test Results"]} />
+                                <Bar dataKey="totalTestResults" fill="#82ca9d" />
+                            </BarChart>
+                        </ResponsiveContainer>
                     </Tab>
                 </Tabs>
             </Card.Body>

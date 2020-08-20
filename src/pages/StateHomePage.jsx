8e801d3a453b5b381fb245417e74b9c084b0c0a3
hoidal <sjhoidal@gmail.com>
2020-08-20 16:21:33 -0600

    added heroku proxy url for api calls due to new CORS error

--- src/pages/StateHomePage.jsx
+++ src/pages/StateHomePage.jsx
@@ -1,64 +1,86 @@
-import React, { Component, Fragment } from 'react'
-import StateHeader from '../components/state-header/StateHeader';
-import SummaryTable from '../components/summary-table/SummaryTable';
-import DataCharts from '../components/data-chart/DataCharts';
-import NewsContainer from '../components/new-container/NewsContainer';
+import React, { Component, Fragment } from "react";
+import StateHeader from "../components/state-header/StateHeader";
+import SummaryTable from "../components/summary-table/SummaryTable";
+import DataCharts from "../components/data-chart/DataCharts";
+import NewsContainer from "../components/new-container/NewsContainer";
 
-import { getStateAbbr, getStateName } from '../util/formatters';
+import { getStateAbbr, getStateName } from "../util/formatters";
+
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
 
 class StateHomePage extends Component {
+  state = {
+    stateName: "",
+    stateCurrentSummaryData: {},
+    stateHistoricalDataSummary: [],
+    stateMetaData: {},
+    newsData: {},
+  };
 
-    state = {
-        stateName: '',
-        stateCurrentSummaryData: {},
-        stateHistoricalDataSummary: [],
-        stateMetaData: {},
-        newsData: {}
+  componentDidMount() {
+    const state = getStateAbbr(this.props.match.params.stateName);
+    const newsApiHeader = {
+      headers: {
+        "Subscription-Key": "a3d734750927483ea4040506f463681e",
+      },
     };
 
-    componentDidMount() {
-        const state = getStateAbbr(this.props.match.params.stateName);
-        const newsApiHeader = {
-            headers: {
-              'Subscription-Key': 'a3d734750927483ea4040506f463681e'
-            },
-        };
+    Promise.all([
+      fetch(
+        `${PROXY_URL}https://covidtracking.com/api/v1/states/${state}/current.json`
+      ),
+      fetch(
+        `${PROXY_URL}https://covidtracking.com/api/v1/states/${state}/daily.json`
+      ),
+      fetch(
+        `${PROXY_URL}https://covidtracking.com/api/v1/states/${state}/info.json`
+      ),
+    ])
+      .then(([res1, res2, res3]) =>
+        Promise.all([res1.json(), res2.json(), res3.json()])
+      )
+      .then(([data1, data2, data3]) =>
+        this.setState({
+          stateName: getStateName(state.toUpperCase()),
+          stateCurrentSummaryData: data1,
+          stateHistoricalDataSummary: data2,
+          stateMetaData: data3,
+        })
+      );
+    fetch(
+      `https://api.smartable.ai/coronavirus/news/US-${state.toUpperCase()}`,
+      newsApiHeader
+    )
+      .then((res) => res.json())
+      .then((data) => this.setState({ newsData: data }));
+  }
 
-        Promise.all([
-            fetch(`https://covidtracking.com/api/v1/states/${state}/current.json`),
-            fetch(`https://covidtracking.com/api/v1/states/${state}/daily.json`),
-            fetch(`https://covidtracking.com/api/v1/states/${state}/info.json`)
-        ])
-        .then(([res1, res2, res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
-        .then(([data1, data2, data3]) => this.setState({
-            stateName: getStateName(state.toUpperCase()),
-            stateCurrentSummaryData: data1,
-            stateHistoricalDataSummary: data2,
-            stateMetaData: data3
-        }))
-        fetch(`https://api.smartable.ai/coronavirus/news/US-${state.toUpperCase()}`, newsApiHeader)
-        .then(res => res.json())
-        .then(data => this.setState({ newsData: data }))
-    }
-
-    render() {
-        const newsContainer = (
-            this.state.newsData.news 
-            ? this.state.newsData.news.length === 0 
-                ? null
-                : <NewsContainer data={this.state.newsData}/> 
-            : null
-        );
-        const hasLocalNews = this.state.newsData.location ? this.state.newsData.news : null;
-        return (
-            <Fragment>
-                <StateHeader stateName={this.state.stateName} info={this.state.stateMetaData} news={hasLocalNews} />
-                <SummaryTable data={this.state.stateCurrentSummaryData} />
-                <DataCharts data={this.state.stateHistoricalDataSummary.sort((a, b) => a.date - b.date)} />
-                {newsContainer}
-            </Fragment>
-        )
-    }
+  render() {
+    const newsContainer = this.state.newsData.news ? (
+      this.state.newsData.news.length === 0 ? null : (
+        <NewsContainer data={this.state.newsData} />
+      )
+    ) : null;
+    const hasLocalNews = this.state.newsData.location
+      ? this.state.newsData.news
+      : null;
+    return (
+      <Fragment>
+        <StateHeader
+          stateName={this.state.stateName}
+          info={this.state.stateMetaData}
+          news={hasLocalNews}
+        />
+        <SummaryTable data={this.state.stateCurrentSummaryData} />
+        <DataCharts
+          data={this.state.stateHistoricalDataSummary.sort(
+            (a, b) => a.date - b.date
+          )}
+        />
+        {newsContainer}
+      </Fragment>
+    );
+  }
 }
 
 export default StateHomePage;
--- src/pages/USHomePage.jsx
+++ src/pages/USHomePage.jsx
@@ -1,62 +1,74 @@
-import React, { Component, Fragment } from 'react';
-import styles from './USHomePage.module.css';
+import React, { Component, Fragment } from "react";
+import styles from "./USHomePage.module.css";
 
-import SummaryTable from '../components/summary-table/SummaryTable';
-import DataCharts from '../components/data-chart/DataCharts';
-import StateDataTable from '../components/data-table/StateDataTable';
-import NewsContainer from '../components/new-container/NewsContainer';
+import SummaryTable from "../components/summary-table/SummaryTable";
+import DataCharts from "../components/data-chart/DataCharts";
+import StateDataTable from "../components/data-table/StateDataTable";
+import NewsContainer from "../components/new-container/NewsContainer";
 
-const US_COVID_SUMMARY_URL = 'https://covidtracking.com/api/v1/us/current.json';
-const US_DAILY_COVID_URL = 'https://covidtracking.com/api/v1/us/daily.json';
-const STATE_COVID_SUMMARY_URL = 'https://covidtracking.com/api/v1/states/current.json';
-const COVID_NEWS_URL = 'https://api.smartable.ai/coronavirus/news/US';
+const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const US_COVID_SUMMARY_URL = "https://covidtracking.com/api/v1/us/current.json";
+const US_DAILY_COVID_URL = "https://covidtracking.com/api/v1/us/daily.json";
+const STATE_COVID_SUMMARY_URL =
+  "https://covidtracking.com/api/v1/states/current.json";
+const COVID_NEWS_URL = "https://api.smartable.ai/coronavirus/news/US";
 
 const NEWS_API_HEADER = {
-    headers: {
-      'Subscription-Key': 'a3d734750927483ea4040506f463681e'
-    }
+  headers: {
+    "Subscription-Key": "a3d734750927483ea4040506f463681e",
+  },
 };
 
 class NationalSummaryPage extends Component {
+  state = {
+    nationalCurrentDataSummary: {},
+    nationalHistoricalDataSummary: [],
+    stateCurrentDataSummary: [],
+    newsData: {},
+  };
 
-    state = {
-        nationalCurrentDataSummary: {},
-        nationalHistoricalDataSummary: [],
-        stateCurrentDataSummary: [],
-        newsData: {}
-    }
+  componentDidMount() {
+    Promise.all([
+      fetch(PROXY_URL + US_COVID_SUMMARY_URL),
+      fetch(PROXY_URL + US_DAILY_COVID_URL),
+      fetch(PROXY_URL + STATE_COVID_SUMMARY_URL),
+      fetch(PROXY_URL + COVID_NEWS_URL, NEWS_API_HEADER),
+    ])
+      .then(([res1, res2, res3, res4]) =>
+        Promise.all([res1.json(), res2.json(), res3.json(), res4.json()])
+      )
+      .then(([data1, data2, data3, data4]) =>
+        this.setState({
+          nationalCurrentDataSummary: data1[0],
+          nationalHistoricalDataSummary: data2,
+          stateCurrentDataSummary: data3,
+          newsData: data4,
+        })
+      );
+  }
 
-    componentDidMount() {
-        Promise.all([
-            fetch(US_COVID_SUMMARY_URL),
-            fetch(US_DAILY_COVID_URL),
-            fetch(STATE_COVID_SUMMARY_URL),
-            fetch(COVID_NEWS_URL, NEWS_API_HEADER)
-        ])
-        .then(([res1, res2, res3, res4]) => Promise.all([res1.json(), res2.json(), res3.json(), res4.json()]))
-        .then(([data1, data2, data3, data4]) => this.setState({
-            nationalCurrentDataSummary: data1[0],
-            nationalHistoricalDataSummary: data2,
-            stateCurrentDataSummary: data3,
-            newsData: data4
-        }))
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <div className={styles.titleContainer}>
-                    <h2 className={styles.headerTitle}>U.S. COVID-19 Summary</h2>
-                    <a href="#newsSection">Recent COVID-19 News</a><br/>
-                    <a href="https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html">CDC Website</a>
-                </div>
-                <SummaryTable data={this.state.nationalCurrentDataSummary} />
-                <DataCharts data={this.state.nationalHistoricalDataSummary.sort((a, b) => a.date - b.date)} />
-                <StateDataTable data={this.state.stateCurrentDataSummary} />
-                <NewsContainer data={this.state.newsData} />
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.headerTitle}>U.S. COVID-19 Summary</h2>
+          <a href="#newsSection">Recent COVID-19 News</a>
+          <br />
+          <a href="https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/cases-in-us.html">
+            CDC Website
+          </a>
+        </div>
+        <SummaryTable data={this.state.nationalCurrentDataSummary} />
+        <DataCharts
+          data={this.state.nationalHistoricalDataSummary.sort(
+            (a, b) => a.date - b.date
+          )}
+        />
+        <StateDataTable data={this.state.stateCurrentDataSummary} />
+        <NewsContainer data={this.state.newsData} />
+      </Fragment>
+    );
+  }
 }
 
 export default NationalSummaryPage;
